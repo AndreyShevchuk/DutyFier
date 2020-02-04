9f7452b12d776f88f5d3e4f476f8e3d0406163fe
blackbearn777 <blackbearn777@gmail.com>
2020-02-04 17:46:18 +0200

    [add] add ViewModel to StatisticsView

--- DutyFier.Client.Wpf/Statistics/StatisticsViewModel.cs
+++ DutyFier.Client.Wpf/Statistics/StatisticsViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DutyFier.Core.Entities;
+using DutyFier.Core.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,22 @@ namespace DutyFier.Client.Wpf.Statistics
 {
     class StatisticsViewModel
     {
+        public List<Person> Persons { get; set; }
+        public List<PersonDutyFeedback> Feedbacks { get; set; }
+
+        public StatisticsViewModel()
+        {
+            Persons = new PersonRepository().GetAll().ToList();
+            Feedbacks = new PersonDutyFeedbackRepository().GetAll().ToList();
+            //InizializeScore();
+        }
+
+        //moved from old project
+
+        //private void InizializeScore()
+        //{
+        //    for (int i = 0; i < Persons.Count; i++)
+        //        Persons[i].Score = Feedbacks.Where(a => a.Person.Equals(Persons[i])).Select(a => a.Scrore).Sum();
+        //}
     }
 }
