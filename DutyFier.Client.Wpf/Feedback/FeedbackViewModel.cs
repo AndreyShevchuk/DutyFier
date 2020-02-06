cdd4a84b9f06248ad18879f8f730c72494cd2006
blackbearn777 <blackbearn777@gmail.com>
2020-02-06 16:19:21 +0200

    [add] add person/type/position viewModel

--- DutyFier.Client.Wpf/Feedback/FeedbackViewModel.cs
+++ DutyFier.Client.Wpf/Feedback/FeedbackViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DutyFier.Core.Entities;
+using DutyFier.Core.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,10 @@ namespace DutyFier.Client.Wpf.Feedback
 {
     class FeedbackViewModel
     {
+        public List<PersonDutyFeedback> feedbacks;
+        public FeedbackViewModel()
+        {
+            feedbacks = new PersonDutyFeedbackRepository().GetAll().ToList();
+        }
     }
 }
--- DutyFier.Client.Wpf/Settings/AddPositionViewModel.cs
+++ DutyFier.Client.Wpf/Settings/AddPositionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using DutyFier.Core.Entities;
+using DutyFier.Core.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,14 @@ namespace DutyFier.Client.Wpf.Settings
 {
     class AddPositionViewModel
     {
-        Position _position;
+        public Position  _position;
+        public List<DutyType> dutyTypes;
         public AddPositionViewModel()
         {
             _position = new Position();
+            dutyTypes = new DutyTypeRepository().GetAll().ToList();
         }
+        public List<DutyType> DutyTypes { get; }
         public DutyType SelectedDutyType { get { return _position.DutyType; } set { _position.DutyType = value; } }
         public string Name { get { return _position.Name; } set { _position.Name = value; } }
         public double Weight { get { return _position.Weight; } set { _position.Weight = value; } }
