6668c792bcc1936c3eb035cf5bb8fb5f1fc9c54f
blackbearn777 <blackbearn777@gmail.com>
2020-02-06 16:56:28 +0200

    [add] hard adding a settingsViewModel (WARNING)

--- DutyFier.Client.Wpf/Settings/SettingsView.xaml.cs
+++ DutyFier.Client.Wpf/Settings/SettingsView.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿
+using DutyFier.Client.Wpf.Settings.Infrastructure;
+using DutyFier.Core.Entities;
+
 using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DutyFier.Client.Wpf.Settings
 {
@@ -20,9 +12,51 @@ namespace DutyFier.Client.Wpf.Settings
     /// </summary>
     public partial class SettingsView : UserControl
     {
+        private SettingsViewModel personDutyViewModel;
         public SettingsView()
         {
+            personDutyViewModel = new SettingsViewModel();
             InitializeComponent();
+            this.DataContext = personDutyViewModel;
         }
+
+        private void CreateDataGrid()
+        {
+            this.dataGrid.AutoGenerateColumns = false;
+            this.dataGrid.CanUserAddRows = false;
+
+            /* Add a column for the displaying the full name of the user */
+            this.dataGrid.Columns.Add(new DataGridTextColumn()
+            {
+                Header = "Person",
+                Binding = new Binding("LastName")
+                {
+                    Mode = BindingMode.OneWay
+                }
+            });
+
+            /* Add a column for each group */
+            foreach (Position position in personDutyViewModel.Positions)
+            {
+                //DataGridCheckBoxColumn chkBoxColumn = new DataGridCheckBoxColumn();
+
+                GroupDataGridCheckBoxColumn chkBoxColumn =
+                    new GroupDataGridCheckBoxColumn(position, personDutyViewModel);
+                chkBoxColumn.Header = position.Name;
+
+                Binding binding = new Binding("Positions");
+                PositionToBooleanConverter converter = new PositionToBooleanConverter();
+                binding.Converter = (IValueConverter)converter;
+                binding.ConverterParameter = position;
+                binding.Mode = BindingMode.OneWay;
+                chkBoxColumn.Binding = binding;
+
+                this.dataGrid.Columns.Add(chkBoxColumn);
+            }
+
+            /* Bind the ItemsSource property of the DataGrid to the Users collection */
+            this.dataGrid.SetBinding(DataGrid.ItemsSourceProperty, "Persons");
+        }
+
     }
 }
---
+++ DutyFier.Client.Wpf/Settings/SettingsViewModel.cs
@@ -0,0 +1,73 @@
+﻿using DutyFier.Client.Wpf.Settings.Infrastructure;
+using DutyFier.Core.Entities;
+using DutyFier.Core.Models;
+using DutyFier.Core.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DutyFier.Client.Wpf.Settings
+{
+    class SettingsViewModel
+    {
+        public readonly DelegateCommand _addOrRemoveGroupCommand;
+        private DutyFierContext context;
+        public PersonRepository personRepository { get; set; }
+        public List<Person> Persons { get; set; }
+        public List<Position> Positions { get; set; }
+
+        public SettingsViewModel()
+        {
+            personRepository = new PersonRepository();
+            _addOrRemoveGroupCommand = new DelegateCommand(AddOrRemoveGroup);
+            context = new DutyFierContext();
+            Persons = context.Persons.ToList();
+            Positions = context.Positions.ToList();
+        }
+
+        public DelegateCommand AddOrRemoveGroupCommand
+        {
+            get { return _addOrRemoveGroupCommand; }
+        }
+
+        private void AddOrRemoveGroup(object parameter)
+        {
+            object[] parameters = parameter as object[];
+            if (parameters == null)
+                throw new ArgumentNullException("parameters");
+            if (!parameters.Length.Equals(3))
+                throw new ArgumentException("Invalid number of arguments.", "parameters");
+            if (!(parameters[0] is bool))
+                throw new ArgumentException("First argument is invalid.", "parameters");
+
+            Person person = parameters[1] as Person;
+            if (person == null)
+                throw new ArgumentException("Second argument is invalid.", "parameters");
+
+            Position position = parameters[2] as Position;
+            if (position == null)
+                throw new ArgumentException("Third argument is invalid.", "parameters");
+
+            bool isAdd = Convert.ToBoolean(parameters[0]);
+            bool existsInCollecton = person.Positions.Contains(position);
+            if (isAdd && !existsInCollecton)
+                person.Positions.Add(position);
+            else if (!isAdd && existsInCollecton)
+                person.Positions.Remove(position);
+        }
+
+        public void SaveChange()
+        {
+            foreach (var item in Persons)
+            {
+                context.Entry(item).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+    }
+}
+
