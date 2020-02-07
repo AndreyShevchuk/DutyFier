f912c29a27c8f9d1a5a41c2a28cc4359d1c7d643
Shevchyk Andrei <medikk1974@gmail.com>
2020-02-07 15:14:11 +0200

    [cm] ?

--- DutyFier.Client.Wpf/Generate/SelectDatesViewModel.cs
+++ DutyFier.Client.Wpf/Generate/SelectDatesViewModel.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using DutyFier.Core.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DutyFier.Client.Wpf.Generate
 {
-    class SelectDatesViewModel
+    class SelectDatesViewModel : INotifyPropertyChanged
     {
+        public Dictionary<Position, ObservableCollection<DateTime>> DatesPosition { get; set; }
+
+        private ObservableCollection<DateTime> selectDates;
+        public ObservableCollection<DateTime> SelectDates { 
+            get 
+            {
+                return selectDates;
+            }
+            set 
+            {
+                selectDates = value;
+                OnPropertyChanged("SelectDates");
+            }
+        }
+
+        private Position selectPosition;
+        public Position SelectPosition
+        {
+            get
+            {
+                return selectPosition;
+            }
+            set
+            {
+                selectPosition = value;
+                OnPropertyChanged("SelectPosition");
+                //out ObservableCollection<DateTime> temp;
+                //DatesPosition.TryGetValue(value,out temp);
+                //SelectDates = temp;
+            }
+        }
+
+
+        public SelectDatesViewModel()
+        {
+            DatesPosition = new Dictionary<Position, ObservableCollection<DateTime>>();
+            DatesPosition.Add(new Position() { Name = "Kpp" }, new ObservableCollection<DateTime>());
+            DatesPosition.Add(new Position() { Name = "Tsp" }, new ObservableCollection<DateTime>());
+            DatesPosition.Add(new Position() { Name = "isi2" }, new ObservableCollection<DateTime>());
+            DatesPosition.Add(new Position() { Name = "isi3" }, new ObservableCollection<DateTime>());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
