36bb5b9547e42e57bd5c906ff9202438dad26532
blackbearn777 <blackbearn777@gmail.com>
2020-02-25 15:49:22 +0200

    [ficha] add AcceptFeedbackView

---
+++ DutyFier.Client.Wpf/Feedback/AcceptFeedbackView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DutyFier.Client.Wpf.Feedback
+{
+    /// <summary>
+    /// Логика взаимодействия для AcceptFeedbackView.xaml
+    /// </summary>
+    public partial class AcceptFeedbackView : Window
+    {
+        public AcceptFeedbackView()
+        {
+            InitializeComponent();
+        }
+    }
+}
