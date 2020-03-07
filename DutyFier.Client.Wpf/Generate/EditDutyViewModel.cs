74213044d7738a1aac97ad597b4d1f3bc2abb550
Shevchyk Andrei <medikk1974@gmail.com>
2020-03-07 21:51:46 +0200

    [fix] Add executor and Remuve in edit Duty

--- DutyFier.Client.Wpf/Generate/EditDutyViewModel.cs
+++ DutyFier.Client.Wpf/Generate/EditDutyViewModel.cs
@@ -1,4 +1,6 @@
-﻿using DutyFier.Core.Entities;
+﻿using DutyFier.Core;
+using DutyFier.Core.Entities;
+using DutyFier.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using Unity;
 
 namespace DutyFier.Client.Wpf.Generate
 {
@@ -37,20 +40,31 @@ namespace DutyFier.Client.Wpf.Generate
         public EditDutyViewModel(Duty duty, IEnumerable<Person> persons)
         {
             ChangeDuty = duty;
-
             FullPersonsDuty = new ObservableCollection<Person>(duty.Executors.Select(e => e.Person));
             FullPersons = new ObservableCollection<Person>(persons);
             MetodTest(FullPersonsDuty, FullPersons);
-            AddPersonComand = new RelayCommands(() => { Test.Add(new MyDictionary(new Person(), FullPersons)); }, () => true);
-            DelPersonComand = new RelayCommands<MyDictionary>(obj => Test.Remove(obj), obj => true);
-            
+            AddPersonComand = new RelayCommands(AddExecutor, () => true);
+            DelPersonComand = new RelayCommands<MyDictionary>(RemuveExecutor, obj => true);
             OkComand = new RelayCommands(ChangeExecutor, () => true);
+        }
+
+        private void RemuveExecutor(MyDictionary myDictionary)
+        {
+            Test.Remove(myDictionary);
+            var delEx = ChangeDuty.Executors.Find(ex => ex.Person.Equals(myDictionary.Key));
+            new ExecuterRepository(MainWindowViewModel.Container.Resolve<DutyFierContext>()).Delete(delEx);
+            ChangeDuty.Executors.Remove(delEx);
+        }
+        private void AddExecutor()
+        {
+            Test.Add(new MyDictionary(new Person(), FullPersons));
+            ChangeDuty.Executors.Add(new Executor { Position = ChangeDuty.Executors.Count>0? ChangeDuty.Executors.First().Position: null, Duty = ChangeDuty});
         }
 
         private void ChangeExecutor()
         {
             var tresh = ChangeDuty.Executors[0].Position;
-           // ChangeDuty.Executors.Clear();
+           // ChangeDuty.Executors.Clear();  
             for (int i = 0; i < ChangeDuty.Executors.Count; i++)
             {
                 ChangeDuty.Executors[i].Person = Test.ElementAt(i).Key;
