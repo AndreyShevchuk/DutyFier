dbb50bb2464ec9f0bc4bcef4d2bee293b53a29d9
Виталий Гетманенко <Blackbearn777@gmail.com>
2020-03-11 15:38:55 +0200

    [ficha] second paragraph of task list 10/03

---
+++ DutyFier.Core/Models/PersonModel.cs
@@ -0,0 +1,24 @@
+﻿using DutyFier.Core.Entities;
+using DutyFier.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DutyFier.Core.Models
+{
+    public class PersonModel
+    {
+        private IRepository<Person> PersonRepository { get; set; }
+        public PersonModel(IRepository<Person> personRepository)
+        {
+            this.PersonRepository = personRepository;
+        }
+        public List<Person> GetAllPerson() => PersonRepository.GetAll().ToList();
+        public void RemovePerson(Person person) => PersonRepository.Delete(person);
+        public void UpdatePersonDependencyToPosition(Person selectedPerson)
+        {
+            PersonRepository.Update(selectedPerson);
+        }
+    }
+}
